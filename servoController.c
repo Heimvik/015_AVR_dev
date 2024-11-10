a165027e8f0caea552d51ce84ebbfa1f2d506acb
heimsnik <cmheimvik@gmail.com>
2024-11-10 11:19:25 +0100

    PID working

--- servoController.c
+++ servoController.c
@@ -1,45 +1,25 @@
 #include "servoController.h"
 
-/*
-New idea:
-DO NOT use floating-point aritmetics!
-Instad, use two ''levels'':
--Decimal bounds: -127 to 127 represents -1,1
--Total bounds: -2**15-1 to 2**15-1 represents total bounds
-The total value has to be a long int (32 bit signed)
-*/
-
-
-/*
-To be used on variables that one want to convert from a unsigned [0-255] to [-127-127] value
-*/
-int8_t uint8ToInt8(uint8_t variable){
-    return (int8_t)(variable-(MAX_UINT8+1)/2);
-}
-
-uint8_t intToUint8(int16_t variable){
-    return (uint8_t)(variable/MAX_INT8)+(MAX_UINT8+1)/2;
-}
-
 uint8_t updatePidController(pidController* controller, uint8_t setPointRaw, uint8_t processVariableRaw){
-    int8_t setPoint = uint8ToInt8(setPointRaw);
-    int8_t processVariable = uint8ToInt8(processVariableRaw);
-    printf("setPoint:%d setPointRaw:%d processVariable:%d processVariableRaw:%d\n\r",setPoint,setPointRaw,processVariable,processVariableRaw);
+    int16_t setPoint = UPSCALE(UINT8TOINT8(setPointRaw));
+    int16_t processVariable = UPSCALE(UINT8TOINT8(processVariableRaw));
+    //printf("setPoint:%d setPointRaw:%d processVariable:%d processVariableRaw:%d\n\r",setPoint,setPointRaw,processVariable,processVariableRaw);
 
-    int16_t currentError = (int16_t)setPoint - (int16_t)processVariable;
-    printf("currentError:%d\n\r",currentError);
-    int16_t pTerm = (controller->kp)*currentError;
+    int16_t currentError = setPoint - processVariable;
+    //printf("currentError:%d\n\r",currentError);
+    int32_t pTerm = (controller->kp)*currentError;
 
     controller->errorSum += currentError;
-    int16_t iTerm = (controller->ki)*(controller->samplePeriod)*(controller->errorSum);
+    int32_t iTerm = (controller->ki)*(controller->samplePeriod)*(controller->errorSum); 
 
-    int16_t dTerm = ((controller->kd)*(currentError - controller->prevError))/controller->samplePeriod;
-    printf("pTerm:%d iTerm:%d dTerm:%d\n\r",pTerm,iTerm,dTerm);
-    return intToUint8(pTerm+iTerm+dTerm);        
+    int32_t dTerm = ((controller->kd)*(currentError - controller->prevError))/controller->samplePeriod;
+    //printf("pTerm:%d iTerm:%d dTerm:%d\n\r",pTerm,iTerm,dTerm);
+    
+    return INT8TOUINT8((pTerm+iTerm+dTerm)/MAX_INT16);
 }
 
 void displayPidController(uint8_t setPoint, uint8_t processVariable) {
-    system("clear");
+    //system("clear");
 
     const int max_position = 255;
     char line[max_position + 1]; // To store the line representation (0-255 chars)
@@ -59,25 +39,25 @@ void displayPidController(uint8_t setPoint, uint8_t processVariable) {
 }
 
 void testPidController(pidController* controller){
-    uint8_t setPoint = 255;
-    uint8_t processVariable = 0;
+    uint8_t setPoint = 0;
+    uint8_t processVariable = 255;
 
     while (1) {
         processVariable = updatePidController(controller,setPoint, processVariable);
         displayPidController(setPoint,processVariable);
-        usleep(10000);
+        usleep(100000);
     }
 }
 
 
 int main(){
     controller = (pidController){
-        .kp = KP*MAX_INT8,
-        .ki = KI*MAX_INT8,
-        .kd = KD*MAX_INT8,
+        .kp = UPSCALE(KP),
+        .ki = UPSCALE(KI),
+        .kd = UPSCALE(KD),
         .errorSum = 0,
         .prevError = 0,
-        .samplePeriod = (uint8_t)((SAMPLE_PERIOD_MS*MAX_INT8)/1000)
+        .samplePeriod = UPSCALE(SAMPLE_PERIOD)
     };
     printf("kp:%d ki:%d kd:%d samplePeriod:%d\n\r",controller.kp,controller.ki,controller.kd,controller.samplePeriod);
     testPidController(&controller);
--- servoController.h
+++ servoController.h
@@ -12,14 +12,21 @@ All numbers are fractions of this.
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAX_INT 32767
 #define MAX_INT8 127
+#define MAX_INT16 32767
 #define MAX_UINT8 255
-#define SAMPLE_PERIOD_MS 10
+#define SAMPLE_PERIOD 0.079 //Makes almost 10 when upscaled
+
+#define UINT8TOINT8(number) (int8_t)(number-(MAX_UINT8+1)/2)
+#define INT8TOUINT8(number) (uint8_t)(number+(MAX_UINT8+1)/2)
+
+//Scales from normal domain to upscaling, where an INT8 becomes INT16
+#define UPSCALE(number) (int16_t)(number*MAX_INT8)
+#define DOWNSCALE(number) (number/MAX_INT8)
 
 #define KP 0.2
 #define KI 0.1
-#define KD 0.05
+#define KD 0.2
 
 uint32_t frame = 0;
 
